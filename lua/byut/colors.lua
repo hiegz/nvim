969f63a65a21c1507a5553b25c3fe4fd91a0795e
Volodymyr But <git@hiegz.com>
2024-02-11 17:59:26 +0100

    feat: preserve colorscheme selection

---
+++ lua/byut/colors.lua
@@ -0,0 +1,48 @@
+--
+-- colors.lua
+--
+--
+
+-- A list of all available colorschemes
+local colorschemes = vim.fn.getcompletion("", "color")
+
+--- Retrieve the most recently used colorscheme
+-- @return string|nil
+local function retrieve_colorscheme()
+    local file = io.open(vim.fn.stdpath("data") .. "/color", "r")
+    if not file then
+        return nil
+    end
+
+    local data = file:read("*a")
+    file:close()
+
+    local colorscheme, _ = data:gsub("[\n\r]", "")
+    return colorscheme
+end
+
+--- Record the name of the newly applied colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function(info)
+        local colorscheme = info.match
+        if not vim.tbl_contains(colorschemes, colorscheme) then
+            return
+        end
+
+        local file = io.open(vim.fn.stdpath("data") .. "/color", "w")
+        if not file then
+            return
+        end
+
+        file:write(string.format("%s\n\r", colorscheme))
+        file:flush()
+        file:close()
+    end,
+})
+
+local colorscheme = retrieve_colorscheme()
+if colorscheme and vim.tbl_contains(colorschemes, colorscheme) then
+    vim.cmd.colorscheme(colorscheme)
+else
+    vim.cmd.colorscheme("default")
+end
