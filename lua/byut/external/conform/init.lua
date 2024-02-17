eb012df5c78adad6b12291ed3e6c4464f2842209
Volodymyr But <git@hiegz.com>
2024-02-17 12:26:27 +0100

    feat(conform): specify formatters by filetype

--- lua/byut/external/conform/init.lua
+++ lua/byut/external/conform/init.lua
@@ -3,6 +3,18 @@ return {
     config = function()
         require("conform").setup({
             formatters_by_ft = {
+                asm = { "asmfmt" },
+                c = { "clang_format" },
+                cpp = { "clang_format" },
+                cmake = { "cmake_format" },
+                haskell = { { "fourmolu", "ormolu" } },
+                markdown = { { "mdformat", "prettierd", "prettier" } },
+                javascript = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                python = { "black" },
+                rust = { "rustfmt" },
+                go = { "gofmt" },
+                lua = { "stylua" },
                 ["_"] = { "trim_whitespace" },
             },
         })
