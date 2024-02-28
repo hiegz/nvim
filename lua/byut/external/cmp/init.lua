7b3e0f3ced802be29357cb59f5576ffd793ecd32
Volodymyr But <vbut.dev+git@gmail.com>
2024-02-28 14:35:54 +0100

    feat(cmp): integrate LuaSnip

--- lua/byut/external/cmp/init.lua
+++ lua/byut/external/cmp/init.lua
@@ -2,7 +2,13 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
         local cmp = require("cmp")
+
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
                 ["<C-n>"] = function(fallback)
@@ -33,5 +39,6 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lua",
+        "L3MON4D3/LuaSnip",
     },
 }
