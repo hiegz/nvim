0f1b3c37dfea1988ce7575bbc3c19612ddd98525
Volodymyr But <git@hiegz.com>
2024-10-12 23:15:33 +0200

    feat(conform): disable cmake-format

--- lua/byut/external/conform/init.lua
+++ lua/byut/external/conform/init.lua
@@ -6,7 +6,7 @@ return {
                 asm = { "asmfmt" },
                 c = { "clang_format" },
                 cpp = { "clang_format" },
-                cmake = { "cmake_format" },
+                -- cmake = { "cmake_format" },
                 haskell = { "fourmolu", "ormolu" },
                 markdown = { "mdformat", "prettierd", "prettier" },
                 javascript = { "prettierd", "prettier" },
