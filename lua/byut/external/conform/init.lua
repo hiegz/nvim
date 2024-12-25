4f83e96a1def3cd123f3a27e206912d6d50bbf1f
Volodymyr But <git@hiegz.com>
2024-12-25 20:03:44 +0100

    feat(conform): format toml files with prettier

--- lua/byut/external/conform/init.lua
+++ lua/byut/external/conform/init.lua
@@ -13,6 +13,7 @@ return {
                 typescript = { "prettierd", "prettier" },
                 jsonc = { "prettierd", "prettier" },
                 json = { "prettierd", "prettier" },
+                toml = { "prettierd", "prettier" },
                 yaml = { "yamlfmt" },
                 python = { "black" },
                 rust = { "rustfmt" },
