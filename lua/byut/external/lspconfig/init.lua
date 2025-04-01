e7f2b136c13da9ebef1a5058f0bf28f7d4794744
Volodymyr But <git@hiegz.com>
2025-04-01 22:15:01 +0200

    ignore the `ServerCancelled` error from `rust_analyzer`

--- lua/byut/external/lspconfig/init.lua
+++ lua/byut/external/lspconfig/init.lua
@@ -71,6 +71,20 @@ return {
 
             lspconfig[server].setup(server_opts)
         end
+
+        -- Ignore the ServerCancelled error from rust_analyzer
+        for _, method in ipairs({
+            "textDocument/diagnostic",
+            "workspace/diagnostic",
+        }) do
+            local default_diagnostic_handler = vim.lsp.handlers[method]
+            vim.lsp.handlers[method] = function(err, result, context, config)
+                if err ~= nil and err.code == -32802 then
+                    return
+                end
+                return default_diagnostic_handler(err, result, context, config)
+            end
+        end
     end,
     dependencies = {
         "hrsh7th/nvim-cmp",
