4926f6ebea11b1f5951181cd930a9497b4401bb5
Volodymyr But <vbut.dev+git@gmail.com>
2025-04-06 23:05:58 +0200

    configure bash

---
+++ lua/byut/languages/bash/init.lua
@@ -0,0 +1,17 @@
+local mason_registry = require("mason-registry")
+local lspconfig = require("lspconfig")
+
+local function setup_lsp()
+    local server_opts = {}
+    server_opts.on_attach = require("byut.defaults.lsp").on_attach
+    server_opts.capabilities = require("byut.defaults.lsp").capabilities()
+
+    lspconfig["bashls"].setup(server_opts)
+end
+
+local lsp = mason_registry.get_package("bash-language-server")
+if not lsp:is_installed() then
+    lsp:install():on("closed", vim.schedule_wrap(setup_lsp))
+else
+    setup_lsp()
+end
