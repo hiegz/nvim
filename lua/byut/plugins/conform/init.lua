c094bd9fdebf6dea22ed86065bdd59068bf40e3c
Volodymyr But <vbut.dev+git@gmail.com>
2025-04-06 20:10:54 +0200

    do not setup LSP clients or formatters directly in lazy.nvim

--- lua/byut/plugins/conform/init.lua
+++ lua/byut/plugins/conform/init.lua
@@ -1,29 +1,4 @@
 return {
     "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                asm = { "asmfmt" },
-                c = { "clang_format" },
-                cpp = { "clang_format" },
-                java = { "clang_format" },
-                -- cmake = { "cmake_format" },
-                haskell = { "fourmolu", "ormolu" },
-                markdown = { "mdformat", "prettierd", "prettier" },
-                javascript = { "prettierd", "prettier" },
-                typescript = { "prettierd", "prettier" },
-                jsonc = { "prettierd", "prettier" },
-                json = { "prettierd", "prettier" },
-                toml = { "prettierd", "prettier" },
-                yaml = { "yamlfmt" },
-                python = { "black" },
-                rust = { "rustfmt" },
-                go = { "gofmt" },
-                sh = { "shfmt" },
-                lua = { "stylua" },
-                zig = { "zigfmt" },
-                ["_"] = { "trim_whitespace" },
-            },
-        })
-    end,
+    opts = {},
 }
