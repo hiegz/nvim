d1b8b96fa84d0c6a03049ce4a9557d57fa862355
Volodymyr But <vbut.dev+git@gmail.com>
2025-04-07 20:09:32 +0200

    configure java

---
+++ lua/byut/languages/java/settings.lua
@@ -0,0 +1,16 @@
+return {
+    java = {
+        configuration = {
+            updateBuildConfiguration = "interactive",
+        },
+        eclipse = {
+            downloadSources = true,
+        },
+        maven = {
+            downloadSources = true,
+        },
+        format = {
+            enabled = true,
+        },
+    },
+}
