e9110615c0f7050f69b5fb035e0cf72efc307c88
Volodymyr But <volodymyr@byut.dev>
2025-04-19 22:30:06 +0200

    configure php

---
+++ lua/byut/languages/php/init.lua
@@ -0,0 +1,29 @@
+local mason_registry = require("mason-registry")
+local lspconfig = require("lspconfig")
+local conform = require("conform")
+
+local function setup_lsp()
+    local server_opts = {}
+    server_opts.on_attach = require("byut.defaults.lsp").on_attach
+    server_opts.capabilities = require("byut.defaults.lsp").capabilities()
+
+    lspconfig["intelephense"].setup(server_opts)
+end
+
+local lsp = mason_registry.get_package("intelephense")
+if not lsp:is_installed() then
+    lsp:install():on("closed", vim.schedule_wrap(setup_lsp))
+else
+    setup_lsp()
+end
+
+local function setup_formatter()
+    conform.formatters_by_ft.php = { "php-cs-fixer" }
+end
+
+local formatter = mason_registry.get_package("php-cs-fixer")
+if not formatter:is_installed() then
+    formatter:install():on("closed", vim.schedule_wrap(setup_formatter))
+else
+    setup_formatter()
+end
