124ce95b799ad28f132c474c13c4e0bb5d87dc45
Volodymyr But <volodymyr@byut.dev>
2025-04-19 21:07:24 +0200

    add key bindings for code snippets

--- lua/byut/plugins/cmp.lua
+++ lua/byut/plugins/cmp.lua
@@ -2,30 +2,53 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
+                    luasnip.lsp_expand(args.body)
                 end,
             },
-            mapping = cmp.mapping.preset.insert({
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<C-n>"] = function(fallback)
+            mapping = {
+                ["<CR>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.confirm({ select = false })
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i" }),
+                ["<C-n>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     else
                         fallback()
                     end
-                end,
-                ["<C-p>"] = function(fallback)
+                end, { "i" }),
+                ["<C-p>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     else
                         fallback()
                     end
-                end,
-            }),
+                end, { "i" }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            },
             sources = cmp.config.sources({
                 { name = "path", priority = 5 },
                 { name = "nvim_lsp", priority = 4 },
