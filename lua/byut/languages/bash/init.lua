f258c2984155a565243f5fd5ff36a3ddf680347a
Volodymyr But <git@hiegz.com>
2025-05-13 19:12:19 +0200

    format bash scripts with shfmt

--- lua/byut/languages/bash/init.lua
+++ lua/byut/languages/bash/init.lua
@@ -1,6 +1,9 @@
 local lspconfig = require("lspconfig")
+local conform = require("conform")
 
 lspconfig["bashls"].setup({
     on_attach = require("byut.defaults.lsp").on_attach,
     capabilities = require("byut.defaults.lsp").capabilities(),
 })
+
+conform.formatters_by_ft.sh = { "shfmt" }
