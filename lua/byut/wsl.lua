9112db26a98d542b7dca1ed18c6c3e01c827bad9
Volodymyr But <git@hiegz.com>
2025-06-20 13:18:13 +0200

    sync with windows clipboard on wsl

--- lua/byut/wsl.lua
+++ lua/byut/wsl.lua
@@ -7,13 +7,15 @@ if not os.getenv("WSL_INTEROP") or not os.getenv("WSL_DISTRO_NAME") then
     return
 end
 
-vim.opt.clipboard = ""
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function(ev)
-        if vim.fn.executable("clip.exe") == 1 then
-            local jobid = vim.fn.jobstart("clip.exe")
-            vim.fn.chansend(jobid, vim.fn.getreg('"'))
-            vim.fn.chanclose(jobid, "stdin")
-        end
-    end,
-})
+vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+        ["+"] = "win32yank -i --crlf",
+        ["*"] = "win32yank -i --crlf",
+    },
+    paste = {
+        ["+"] = "win32yank -o --lf",
+        ["*"] = "win32yank -o --lf",
+    },
+    cache_enabled = true,
+}
