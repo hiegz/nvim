e37038fc5b0c6a6c204f65e72127fe217271516f
Volodymyr But <git@hiegz.com>
2025-08-29 16:06:56 +0200

    configure html lsp

--- lua/byut/languages/html/init.lua
+++ lua/byut/languages/html/init.lua
@@ -1,3 +1,9 @@
+local lspconfig = require("lspconfig")
 local conform = require("conform")
+
+lspconfig["html"].setup({
+    on_attach = require("byut.defaults.lsp").on_attach,
+    capabilities = require("byut.defaults.lsp").capabilities(),
+})
 
 conform.formatters_by_ft.html = { "prettierd", "prettier" }
