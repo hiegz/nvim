3d16642b7a7ac428162a14ee97b9b95e5a507c73
Volodymyr But <git@hiegz.com>
2025-08-29 15:55:15 +0200

    use prettier to format css

--- lua/byut/languages/css/init.lua
+++ lua/byut/languages/css/init.lua
@@ -1,7 +1,10 @@
 local lspconfig = require("lspconfig")
+local conform = require("conform")
 
 lspconfig["cssls"].setup({
     on_attach = require("byut.defaults.lsp").on_attach,
     capabilities = require("byut.defaults.lsp").capabilities(),
     settings = require("byut.languages.css.settings"),
 })
+
+conform.formatters_by_ft.css = { "prettierd", "prettier" }
