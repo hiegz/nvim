f790c2a4c5d5f11c3502b9caa745e31c999ee288
Volodymyr But <git@hiegz.com>
2025-09-01 14:58:01 +0200

    configure tailwindcss lsp

--- lua/byut/languages/html/init.lua
+++ lua/byut/languages/html/init.lua
@@ -6,6 +6,11 @@ lspconfig["html"].setup({
     capabilities = require("byut.defaults.lsp").capabilities(),
 })
 
+lspconfig["tailwindcss"].setup({
+    on_attach = require("byut.defaults.lsp").on_attach,
+    capabilities = require("byut.defaults.lsp").capabilities(),
+})
+
 conform.formatters_by_ft.html = { "prettierd", "prettier" }
 
 -- Treat Handlebars files as HTML
